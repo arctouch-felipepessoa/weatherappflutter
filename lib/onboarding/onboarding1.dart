b868c58a1db6764e0dbf7213321168997ee32e14
Felipe Pessoa <felipepessoa@Felipes-MacBook-Pro.local>
2019-06-03 17:25:11 -0300

    atw-1, atw-2

---
+++ lib/onboarding/onboarding1.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class OnBoarding01 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("text1");
+  }
+
+}
---
+++ lib/onboarding/onboarding2.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class OnBoarding02 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("text2");
+  }
+
+}
